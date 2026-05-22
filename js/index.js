13a0af926888a7ff419e4733ae5a6d2040864e66
Viacheslav <slava19092009@gmail.com>
2026-05-22 11:52:30 +0500

    add burger menu and dictionary logic

--- js/index.js
+++ js/index.js
@@ -1,4 +1,11 @@
 const trigger = document.querySelectorAll(".faq__trigger");
+const lettersTrigger = document.querySelectorAll('.letters__trigger')
+const burger = document.querySelector('.header__burger')
+const burgerList = document.querySelector('.header__list')
+
+burger.addEventListener('click', () => {
+  burgerList.classList.toggle('active')
+})
 
 trigger.forEach((trigger) => {
   trigger.addEventListener("click", () => {
@@ -15,3 +22,18 @@ trigger.forEach((trigger) => {
     currentArrow.classList.toggle("active");
   });
 });
+
+lettersTrigger.forEach((trigger) => {
+  trigger.addEventListener("click", () => {
+    const infoBlock = trigger.nextElementSibling;
+    if (infoBlock) {
+      if (infoBlock.textContent === "") {
+        console.log("В разделе нет информации");
+      } else {
+        infoBlock.classList.toggle("active");
+      }
+    }
+  });
+});
+
+
