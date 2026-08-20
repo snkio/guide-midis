f4f85cb7640bd22d002279bbd8b80d124a300a30
Viacheslav <snkio.dev@gmail.com>
2026-08-20 12:55:16 +0500

    fix: remove duplicate text from alt attr.

--- js/index.js
+++ js/index.js
@@ -24,11 +24,11 @@ trigger.forEach((trigger) => {
         console.log("В разделе нет информации");
       } else {
         infoBlock.classList.toggle("active");
+
+        const currentArrow = trigger.querySelector(".faq__arrow");
+        currentArrow.classList.toggle("active");
       }
     }
-
-    const currentArrow = trigger.querySelector(".faq__arrow");
-    currentArrow.classList.toggle("active");
   });
 });
 
